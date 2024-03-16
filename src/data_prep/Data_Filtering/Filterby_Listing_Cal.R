e7954a6e25d9f5c04e8f87481c7be7eb56c67368
zeynep <zeynep@zeyneps-laptop.kpn>
2024-03-16 23:40:41 +0100

    organizing files zeynep

--- src/data_prep/Data_Filtering/Filterby_Listing_Cal.R
+++ src/data_prep/Data_Filtering/Filterby_Listing_Cal.R
@@ -7,9 +7,13 @@ listings=read_csv("listings.csv")
 library(data.table)
 library(tidyverse)
 
+listings = download.file('http://data.insideairbnb.com/spain/catalonia/barcelona/2023-12-13/visualisations/listings.csv', 'listings.csv')
+calendar = download.file('http://data.insideairbnb.com/spain/catalonia/barcelona/2023-12-13/data/calendar.csv.gz', 'calender.csv')
+
 #upload the files
 calendar=read_csv("calendar.csv")
 setDT(calendar)
 listings<- read_csv("listings.csv")
 setDT(listings)
+
 
