4b27087af59344a35eb54a7885b80069da021b09
RebeccaKeijzer <you@example.com>
2024-03-23 00:49:50 +0100

    the updated clean.R file

--- src/analysis/regression_analysis.R
+++ src/analysis/regression_analysis.R
@@ -13,6 +13,40 @@ mean_price_during
 mean_price_off <- mean(off_festival$price, na.rm = TRUE)
 mean_price_off
 
+# Calculate summary statistics of mean_price_during
+min_value_during <- min(during_festival$price, na.rm = TRUE)
+q1_value_during <- quantile(during_festival$price, probs = 0.25, na.rm = TRUE)
+median_value_during <- median(during_festival$price, na.rm = TRUE)
+mean_value_during <- mean(during_festival$price, na.rm = TRUE)
+q3_value_during <- quantile(during_festival$price, probs = 0.75, na.rm = TRUE)
+max_value_during <- max(during_festival$price, na.rm = TRUE)
+
+# Calculate summary statistics of mean_price_off
+min_value_off <- min(off_festival$price, na.rm = TRUE)
+q1_value_off <- quantile(off_festival$price, probs = 0.25, na.rm = TRUE)
+median_value_off <- median(off_festival$price, na.rm = TRUE)
+mean_value_off <- mean(off_festival$price, na.rm = TRUE)
+q3_value_off <- quantile(off_festival$price, probs = 0.75, na.rm = TRUE)
+max_value_off <- max(off_festival$price, na.rm = TRUE)
+
+# Convert percentiles to numerical values
+q1_value_during <- as.numeric(q1_value_during)
+q3_value_during <- as.numeric(q3_value_during)
+q1_value_off <- as.numeric(q1_value_off)
+q3_value_off <- as.numeric(q3_value_off)
+
+# Create a data frame with summary statistics for mean_price_during and mean_price_off
+summary_df_during_off <- data.frame(
+  Variable = c("during", "off"),
+  Mean = c(mean_value_during, mean_value_off),
+  Median = c(median_value_during, median_value_off),
+  Min = c(min_value_during, min_value_off),
+  Max = c(max_value_during, max_value_off),
+  Q1 = c(q1_value_during, q1_value_off),
+  Q3 = c(q3_value_during, q3_value_off)
+)
+
+
 # estimate a linear regression with price and during and off festival
 price_off_during <- lm(price~ dataset, data = combined_price)
 summary(price_off_during)
@@ -30,7 +64,7 @@ summary(minimum_nights_off_festival)
 minimum_nights_during_festival <- lm(price ~ minimum_nights, data = during_festival)
 summary(minimum_nights_during_festival)
 
-save(mean_price_during,mean_price_off,price_off_during,everything_during_festival,minimum_nights_off_festival,minimum_nights_during_festival,file="../../gen/output/model_results.RData")
+save(mean_price_during,mean_price_off,price_off_during,summary_df_during_off,everything_during_festival,minimum_nights_off_festival,minimum_nights_during_festival,file="../../gen/output/model_results.RData")
 
 
 
